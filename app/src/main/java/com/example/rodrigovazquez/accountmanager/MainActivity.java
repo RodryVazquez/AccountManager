895f8336850a391945842a82f8703e3047c221f7
Rodrigo Vazquez <rodrigo@rodrigovazquez.com.mx>
2017-05-15 02:11:23 -0600

    Project

---
+++ app/src/main/java/com/example/rodrigovazquez/accountmanager/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.rodrigovazquez.accountmanager;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView name, email, gender;
+    private ImageView imageProfile;
+    String textName, textEmail, textGender, textBirthday, userImageUrl;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageProfile = (ImageView)findViewById(R.id.profileImage);
+        name = (TextView)findViewById(R.id.txtNameValue);
+        email = (TextView)findViewById(R.id.txtEmailValue);
+        gender = (TextView)findViewById(R.id.txtGenderValue);
+
+        //Obtenemos el email del usuario
+        //atravez del intent que inicia la actividad
+        Intent intent = getIntent();
+        textEmail = intent.getStringExtra("email_id");
+        System.out.print(textEmail);
+        email.setText(textEmail);
+    }
+}
